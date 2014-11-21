9748b03d2dd4cbac256d365cf7da1e9f3c858ed2
Peter Braswell <pbraswell@heroku.com>
2014-11-21 12:14:24 -0500

    change directory that servers application

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(gzippo.staticGzip("" + __dirname + "/app"));
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
 console.log("Listening on " + port);
