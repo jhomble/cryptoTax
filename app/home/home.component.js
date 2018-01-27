9dbf2a06594a826d3572ada137e42ea12b54f952
jhomble <jhomble7@gmail.com>
2018-01-27 18:19:34 -0500

    fixing ctrl

--- app/home/home.component.js
+++ app/home/home.component.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('crpto.home', ['ngRoute'])
+angular.module('crypto.home', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/home', {
