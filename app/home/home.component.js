76321327caba2c62eed338993b2c55e56b89090f
jhomble <jhomble7@gmail.com>
2018-01-27 18:12:26 -0500

    clean up template

---
+++ app/home/home.component.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('crpto.home', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {
+    templateUrl: 'home/home.html',
+    controller: 'homeCtrl'
+  });
+}])
+
+.controller('homeCtrl', [function() {
+
+}]);
